255bcdf5395bfc457e2b238cd829dead3e333dbd
franciscolir <francisco.lira.b@gmail.com>
2025-05-21 21:00:50 -0400

    agrega opciones para que se pueda acceder desde cualquier ip

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = require('./app/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+//app.listen(PORT, () => {
+  //console.log(`Servidor corriendo en http://localhost:${PORT}`);
+//});
+
+//agrega opciones para que se pueda acceder desde cualquier ip
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+}); 
